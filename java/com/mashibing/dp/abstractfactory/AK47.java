c0e09e2a1aa1aef62a4d7395bb3c663b94c5d1fe
liming08990434 <875915922@qq.com>
2019-12-03 16:08:49 +0800

    设计模式继续

--- java/com/mashibing/dp/abstractfactory/AK47.java
+++ java/com/mashibing/dp/abstractfactory/AK47.java
@@ -2,6 +2,6 @@ package com.mashibing.dp.abstractfactory;
 
 public class AK47 extends Weapon{
     public void shoot() {
-        System.out.println("tutut33utu2tu2....");
+        System.out.println("tutut33utu2tu2fff....");
     }
 }
